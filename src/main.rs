78527105f84c8300f93d6ae0afa8f32c98bdee6a
Christof Laenzlinger <laenzlinger@github.com>
2023-02-08 22:33:13 +0100

    Add USB_BUS

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,17 @@ mod app {
         hal::{
             clocks::init_clocks_and_plls,
             gpio::{Pin, PushPullOutput},
+            usb::UsbBus,
             Sio, Watchdog,
         },
         Pins,
     };
+
+    use usb_device::class_prelude::UsbBusAllocator;
+
     const XTAL_FREQ_HZ: u32 = 12_000_000;
+
+    static mut USB_BUS: Option<usb_device::bus::UsbBusAllocator<UsbBus>> = None;
 
     #[monotonic(binds = TIMER_IRQ_0, default = true)]
     type MyMono = Rp2040Monotonic;
@@ -34,10 +40,9 @@ mod app {
     #[init]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         let mut resets = cx.device.RESETS;
-
         let mut watchdog = Watchdog::new(cx.device.WATCHDOG);
 
-        let _clocks = init_clocks_and_plls(
+        let clocks = init_clocks_and_plls(
             XTAL_FREQ_HZ,
             cx.device.XOSC,
             cx.device.CLOCKS,
@@ -61,6 +66,22 @@ mod app {
         );
 
         let led = pins.led.into_push_pull_output();
+
+        // The bus that is used to manage the device and class below.
+        let usb_bus = UsbBusAllocator::new(UsbBus::new(
+            cx.device.USBCTRL_REGS,
+            cx.device.USBCTRL_DPRAM,
+            clocks.usb_clock,
+            true,
+            &mut resets,
+        ));
+
+        // We store the bus in a static to make the borrows satisfy the rtic model, since rtic
+        // needs all references to be 'static.
+        unsafe {
+            USB_BUS = Some(usb_bus);
+        }
+
         blink::spawn().unwrap();
         (Shared {}, Local { led }, init::Monotonics(mono))
     }
