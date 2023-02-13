fe5326e9d135e5241e9a869352266b16feb5eac8
Christof Laenzlinger <laenzlinger@github.com>
2023-02-13 22:41:08 +0100

    Prototype Rotary encoder

--- src/mmi.rs
+++ src/mmi.rs
@@ -1,13 +1,12 @@
 use debouncr::{debounce_stateful_5, DebouncerStateful, Repeat5};
 use embedded_hal::digital::v2::InputPin;
+use rotary_encoder_embedded::{standard::StandardMode, Direction, RotaryEncoder};
 use rp_pico::hal::gpio::{
     pin::bank0::{Gpio16, Gpio17, Gpio18, Gpio19, Gpio20, Gpio21},
     Input, Pin, PullUp,
 };
-pub enum Direction {
-    Up,
-    Down,
-}
+
+use midi_types::Value7;
 
 pub enum InputEvent {
     ButtonA,
@@ -16,18 +15,20 @@ pub enum InputEvent {
     ButtonD,
     ButtonE,
     ButtonF,
-    ExpessionPedal(midi_types::Value7),
+    ExpessionPedal(Value7),
     VolButton,
-    Vol(Direction),
+    Vol(Value7),
     GainButton,
-    Gain(Direction),
+    Gain(Value7),
 }
 
 pub struct Inputs {
-    vol_sw_pin: Pin<Gpio18, Input<PullUp>>,
     vol_sw_state: DebouncerStateful<u8, Repeat5>,
-    gain_sw_pin: Pin<Gpio21, Input<PullUp>>,
+    vol_sw_pin: Pin<Gpio18, Input<PullUp>>,
+    vol_rotary: RotaryEncoder<StandardMode, Pin<Gpio17, Input<PullUp>>, Pin<Gpio16, Input<PullUp>>>,
+    vol_value: u8,
     gain_sw_state: DebouncerStateful<u8, Repeat5>,
+    gain_sw_pin: Pin<Gpio21, Input<PullUp>>,
 }
 
 impl Inputs {
@@ -44,6 +45,8 @@ impl Inputs {
             vol_sw_state: debounce_stateful_5(false),
             gain_sw_pin,
             gain_sw_state: debounce_stateful_5(false),
+            vol_rotary: RotaryEncoder::new(vol_dt_pin, vol_clk_pin).into_standard_mode(),
+            vol_value: 0,
         }
     }
 
@@ -57,6 +60,22 @@ impl Inputs {
             .update(self.gain_sw_pin.is_high().unwrap());
         if gain_edge.is_some() {
             return Some(InputEvent::GainButton);
+        }
+        self.vol_rotary.update();
+        match self.vol_rotary.direction() {
+            Direction::Clockwise => {
+                if self.vol_value < 127 {
+                    self.vol_value = self.vol_value + 1;
+                }
+                Some(InputEvent::Vol(Value7::new(self.vol_value)));
+            }
+            Direction::Anticlockwise => {
+                if self.vol_value > 1 {
+                    self.vol_value = self.vol_value - 1;
+                }
+                Some(InputEvent::Vol(Value7::new(self.vol_value)));
+            }
+            Direction::None => (),
         }
 
         None
