2e4039df7e48a38ff0473bbbba7401df6a9d6112
Christof Laenzlinger <laenzlinger@github.com>
2023-02-18 23:06:46 +0100

    Only react on activate

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -4,8 +4,11 @@ mod rc500;
 use heapless::Vec;
 use midi_types::MidiMessage;
 
+use crate::hmi::Edge::{Activate, Deactivate};
+
 use self::plethora::Plethora;
 use self::rc500::{RC500Event, RC500};
+use crate::hmi::InputEvent;
 
 pub type MidiMessages = Vec<MidiMessage, 8>;
 
@@ -22,8 +25,14 @@ impl Devices {
 
     pub fn map(&mut self, event: crate::hmi::InputEvent) -> MidiMessages {
         match event {
-            crate::hmi::InputEvent::ButtonA(_) => self.plethora(Plethora::BoardUp),
-            crate::hmi::InputEvent::ButtonB(_) => self.rc500(RC500Event::Mem(rc500::Direction::Up)),
+            InputEvent::ButtonA(e) => match e {
+                Activate => self.plethora(Plethora::BoardUp),
+                Deactivate => Vec::new(),
+            },
+            InputEvent::ButtonB(e) => match e {
+                Activate => self.rc500(RC500Event::Mem(rc500::Direction::Up)),
+                Deactivate => Vec::new(),
+            },
             _ => Vec::new(),
         }
     }
