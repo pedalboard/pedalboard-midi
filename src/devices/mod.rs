18d36505f2dce7a240fde567c11b45137c967fae
Christof Laenzlinger <laenzlinger@github.com>
2023-02-23 16:48:29 +0100

    More effect colors

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -7,7 +7,10 @@ use crate::hmi::inputs::{
 };
 use heapless::Vec;
 use midi_types::MidiMessage;
-use smart_leds::colors::{BLUE, GREEN, ORANGE, RED, SEA_GREEN, VIOLET, WHITE};
+use smart_leds::{
+    colors::{BLUE, GREEN, ORANGE, RED, SEA_GREEN, VIOLET, WHITE},
+    RGB8,
+};
 
 use crate::hmi::leds::{
     Animation::{Off, On},
@@ -83,42 +86,34 @@ impl Devices {
 
     fn map_live_effect(&mut self, event: InputEvent) -> Actions {
         let mut animations: Animations = Vec::new();
+        animations.push(Off(Led::A)).unwrap();
+        animations.push(Off(Led::B)).unwrap();
+        animations.push(Off(Led::C)).unwrap();
+        animations.push(Off(Led::F)).unwrap();
         match event {
             InputEvent::ButtonA(e) => match e {
                 Activate => {
                     animations.push(On(Led::A, BLUE)).unwrap();
-                    animations.push(Off(Led::B)).unwrap();
-                    animations.push(Off(Led::C)).unwrap();
-                    animations.push(Off(Led::F)).unwrap();
                     Actions::new(self.plethora(PlethoraEvent::GoToBoard(1)), animations)
                 }
                 Deactivate => Actions::default(),
             },
             InputEvent::ButtonB(e) => match e {
                 Activate => {
-                    animations.push(Off(Led::A)).unwrap();
                     animations.push(On(Led::B, SEA_GREEN)).unwrap();
-                    animations.push(Off(Led::C)).unwrap();
-                    animations.push(Off(Led::F)).unwrap();
                     Actions::new(self.plethora(PlethoraEvent::GoToBoard(2)), animations)
                 }
                 Deactivate => Actions::default(),
             },
             InputEvent::ButtonC(e) => match e {
                 Activate => {
-                    animations.push(Off(Led::A)).unwrap();
-                    animations.push(Off(Led::B)).unwrap();
                     animations.push(On(Led::C, GREEN)).unwrap();
-                    animations.push(Off(Led::F)).unwrap();
                     Actions::new(self.plethora(PlethoraEvent::GoToBoard(3)), animations)
                 }
                 Deactivate => Actions::default(),
             },
             InputEvent::ButtonF(e) => match e {
                 Activate => {
-                    animations.push(Off(Led::A)).unwrap();
-                    animations.push(Off(Led::B)).unwrap();
-                    animations.push(Off(Led::C)).unwrap();
                     animations.push(On(Led::F, VIOLET)).unwrap();
                     Actions::new(self.plethora(PlethoraEvent::GoToBoard(4)), animations)
                 }
@@ -127,18 +122,23 @@ impl Devices {
             InputEvent::ButtonD(e) => match e {
                 Activate => Actions::new(
                     self.plethora(PlethoraEvent::Board(Direction::Up)),
-                    NO_ANIMATIONS,
+                    animations,
                 ),
                 Deactivate => Actions::default(),
             },
             InputEvent::ButtonE(e) => match e {
                 Activate => Actions::new(
                     self.plethora(PlethoraEvent::Board(Direction::Down)),
-                    NO_ANIMATIONS,
+                    animations,
                 ),
                 Deactivate => Actions::default(),
             },
             InputEvent::ExpessionPedal(val) => {
+                animations.clear();
+                let v: u8 = val.into();
+                let c = colorous::REDS.eval_rational(v as usize, 127);
+                let color = RGB8::new(c.r, c.g, c.b);
+                animations.push(On(Led::Clip, color)).unwrap();
                 Actions::new(self.plethora(PlethoraEvent::HotKnob(3, val)), NO_ANIMATIONS)
             }
             _ => Actions::default(),
