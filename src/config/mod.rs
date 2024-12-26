6ae4e180ac88afffdb24fe64fbd7df63736f0895
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2024-12-26 18:29:01 +0100

    Use opendesk protocol to reset to usbboot

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,4 +1,7 @@
+use defmt::*;
 use heapless::Vec;
+use opendeck::{OpenDeckMsg, OpenDeckParser, SpecialMsg};
+use rp2040_hal::rom_data::reset_to_usb_boot;
 
 pub const SYSEX_BUFFER_SIZE: usize = 64;
 
@@ -6,30 +9,19 @@ pub type SysexBuffer = Vec<u8, SYSEX_BUFFER_SIZE>;
 
 /// Processes a SysEx request and returns an optional response.
 pub fn process_sysex(request: &[u8]) -> Option<SysexBuffer> {
-    /// Identity request message.
-    ///
-    /// See section *DEVICE INQUIRY* of the *MIDI 1.0 Detailed Specification* for further details.
-    const IDENTITY_REQUEST: [u8; 6] = [0xF0, 0x7E, 0x7F, 0x06, 0x01, 0xF7];
-
-    if request == IDENTITY_REQUEST {
-        let mut response = Vec::<u8, SYSEX_BUFFER_SIZE>::new();
-        response
-            .extend_from_slice(&[
-                0xF0, 0x7E, 0x7F, 0x06, 0x02, // Header
-                0x01, // Manufacturer ID
-                0x02, // Family code
-                0x03, // Family code
-                0x04, // Family member code
-                0x05, // Family member code
-                0x00, // Software revision level
-                0x00, // Software revision level
-                0x00, // Software revision level
-                0x00, // Software revision level
-                0xF7,
-            ])
-            .ok();
-
-        return Some(response);
+    if let Ok(msg) = OpenDeckParser::parse(request) {
+        match msg {
+            OpenDeckMsg::Special(special) => match special {
+                SpecialMsg::BootloaderMode => {
+                    info!("reset to usb boot");
+                    reset_to_usb_boot(0, 0)
+                }
+                SpecialMsg::Reboot => {}
+                _ => {}
+            },
+            OpenDeckMsg::Configuration => {}
+            _ => {}
+        }
     }
 
     None
