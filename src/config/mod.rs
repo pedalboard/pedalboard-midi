f9b102debf840cc4b3c2de897ee03b31ba187547
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2024-12-27 14:11:59 +0100

    Update to latest opendeck version

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,6 +1,6 @@
 use defmt::*;
 use heapless::Vec;
-use opendeck::{OpenDeckMsg, OpenDeckParser, SpecialMsg};
+use opendeck::{parser::OpenDeckParser, OpenDeckRequest, SpecialRequest};
 use rp2040_hal::rom_data::reset_to_usb_boot;
 
 pub const SYSEX_BUFFER_SIZE: usize = 64;
@@ -11,15 +11,15 @@ pub type SysexBuffer = Vec<u8, SYSEX_BUFFER_SIZE>;
 pub fn process_sysex(request: &[u8]) -> Option<SysexBuffer> {
     if let Ok(msg) = OpenDeckParser::parse(request) {
         match msg {
-            OpenDeckMsg::Special(special) => match special {
-                SpecialMsg::BootloaderMode => {
+            OpenDeckRequest::Special(special) => match special {
+                SpecialRequest::BootloaderMode => {
                     info!("reset to usb boot");
                     reset_to_usb_boot(0, 0)
                 }
-                SpecialMsg::Reboot => {}
+                SpecialRequest::Reboot => {}
                 _ => {}
             },
-            OpenDeckMsg::Configuration => {}
+            OpenDeckRequest::Configuration => {}
             _ => {}
         }
     }
