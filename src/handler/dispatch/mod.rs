754d97ea6ba099775952d4d22f0ea7e48c3bf067
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-01-03 17:31:09 +0100

    Add an opendeck handler

--- src/handler/dispatch/mod.rs
+++ src/handler/dispatch/mod.rs
@@ -1,5 +1,6 @@
 mod live_effect;
 mod live_looper;
+mod opendeck;
 mod setup_looper;
 mod test;
 
@@ -11,6 +12,7 @@ use heapless::Vec;
 
 /// Enum based static dispatch
 pub enum HandlerEnum {
+    OpenDeck(self::opendeck::OpenDeck),
     LiveEffect(self::live_effect::LiveEffect),
     LiveLooper(self::live_looper::LiveLooper),
     SetupLooper(self::setup_looper::SetupLooper),
@@ -20,6 +22,7 @@ pub enum HandlerEnum {
 impl Handler for HandlerEnum {
     fn handle_human_input(&mut self, e: InputEvent) -> Actions {
         match self {
+            HandlerEnum::OpenDeck(h) => h.handle_human_input(e),
             HandlerEnum::LiveEffect(h) => h.handle_human_input(e),
             HandlerEnum::LiveLooper(h) => h.handle_human_input(e),
             HandlerEnum::SetupLooper(h) => h.handle_human_input(e),
@@ -28,6 +31,7 @@ impl Handler for HandlerEnum {
     }
     fn leds(&mut self) -> &mut Leds {
         match self {
+            HandlerEnum::OpenDeck(h) => h.leds(),
             HandlerEnum::LiveEffect(h) => h.leds(),
             HandlerEnum::LiveLooper(h) => h.leds(),
             HandlerEnum::SetupLooper(h) => h.leds(),
@@ -39,6 +43,7 @@ impl Handler for HandlerEnum {
 impl fmt::Debug for HandlerEnum {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            HandlerEnum::OpenDeck(_) => f.debug_tuple("OpenDeck").finish(),
             HandlerEnum::LiveEffect(_) => f.debug_tuple("LiveEffect").finish(),
             HandlerEnum::LiveLooper(_) => f.debug_tuple("LiveLooper").finish(),
             HandlerEnum::SetupLooper(_) => f.debug_tuple("SetupLooper").finish(),
