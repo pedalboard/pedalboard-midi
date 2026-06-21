6c2f2e2290b4c3591e92a24678992bb4f222e4d7
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-06-21 20:54:59 +0200

    Button press takes priority over expression pedal on shared rings

--- src/opendeck_handler.rs
+++ src/opendeck_handler.rs
@@ -182,8 +182,12 @@ impl OpenDeck {
             match cc {
                 0 => self.leds.set_ledring(RingAnim::Fill(CYAN, val.min(12)), LedRings::Vol),
                 1 => self.leds.set_ledring(RingAnim::Fill(CYAN, val.min(12)), LedRings::Gain),
-                2 => self.leds.set_ledring(RingAnim::Fill(CYAN, ((val as u16 * 12) / 127) as u8), LedRings::D),
-                3 => self.leds.set_ledring(RingAnim::Fill(CYAN, ((val as u16 * 12) / 127) as u8), LedRings::F),
+                2 => if !self.config.output_state(3) {
+                    self.leds.set_ledring(RingAnim::Fill(CYAN, ((val as u16 * 12) / 127) as u8), LedRings::D);
+                }
+                3 => if !self.config.output_state(5) {
+                    self.leds.set_ledring(RingAnim::Fill(CYAN, ((val as u16 * 12) / 127) as u8), LedRings::F);
+                }
                 _ => {}
             }
         }
@@ -206,23 +210,20 @@ impl OpenDeck {
 
     /// Sync opendeck output states to physical LED rings.
     fn sync_output_leds(&mut self) {
-        // Outputs 0-5 map to buttons A-F, but D(3) and F(5) are used for expression pedals
-        const RING_MAP: [Option<LedRings>; 6] = [
-            Some(LedRings::A),
-            Some(LedRings::B),
-            Some(LedRings::C),
-            None, // D - expression pedal
-            Some(LedRings::E),
-            None, // F - expression pedal
+        const RING_MAP: [LedRings; 6] = [
+            LedRings::A,
+            LedRings::B,
+            LedRings::C,
+            LedRings::D,
+            LedRings::E,
+            LedRings::F,
         ];
         for i in 0..self.config.output_count().min(6) {
-            if let Some(ring) = RING_MAP[i] {
-                let on = self.config.output_state(i);
-                self.leds.set_ledring(
-                    if on { RingAnim::On(GREEN) } else { RingAnim::Off },
-                    ring,
-                );
-            }
+            let on = self.config.output_state(i);
+            self.leds.set_ledring(
+                if on { RingAnim::On(GREEN) } else { RingAnim::Off },
+                RING_MAP[i],
+            );
         }
     }
 }
